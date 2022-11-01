48032386190a5c163cf307d1e5e57a0c306a1dcc
MichalStabrawa <mistaaqua@gmail.com>
2022-11-01 22:48:49 +0100

    add header component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 import NavComponent from './components/NavComponent/NavComponent';
+import HeaderComponent from './components/HeaderComponent/HeaderComponent';
+import Wrapper from './components/UI/Wrapper/Wrapper';
 
 function App() {
   return (
     <div className="App">
       <NavComponent></NavComponent>
+      <Wrapper>
+        <HeaderComponent></HeaderComponent>
+      </Wrapper>
 
     </div>
   );
---
+++ src/components/HeaderComponent/HeaderComponent.js
@@ -0,0 +1,10 @@
+import classes from './HeaderComponent.module.scss'
+
+const HeaderComponent = props => {
+    return (<header className={classes.header}>
+        <h1>Header lorem ipsum</h1>
+        <p>Lorem ipsum description</p>
+    </header>)
+}
+
+export default HeaderComponent
