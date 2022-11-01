7d69f5d4c588933602bc8285d4a0bb3e82dcf9ff
MichalStabrawa <mistaaqua@gmail.com>
2022-11-01 20:33:52 +0100

    navComponent changes UX

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Wrapper from './components/UI/Wrapper';
+
 import NavComponent from './components/NavComponent/NavComponent';
 
 function App() {
   return (
     <div className="App">
       <NavComponent></NavComponent>
-      <header className="App-header">
 
-
-        <Wrapper><h2>Test</h2></Wrapper>
-      </header>
     </div>
   );
 }
