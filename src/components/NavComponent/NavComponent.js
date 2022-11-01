fcb1e099030843f8fe2d56d675aaf12f8e132e6e
MichalStabrawa <mistaaqua@gmail.com>
2022-11-01 13:47:41 +0100

    add logo.png

--- src/components/NavComponent/NavComponent.js
+++ src/components/NavComponent/NavComponent.js
@@ -1,9 +1,10 @@
 import classes from './NavComponent.module.scss';
+import logo from '../../assets/bapp.png';
 
 const NavComponent = props => {
     return (
         <nav className={classes.navbar}>
-            <div className="logo">Logo</div>
+            <div className={classes.logo}><img src={logo} alt="logo" /></div>
             <ul className={classes.nav}>
                 <li className="nav-item">Lorem</li>
                 <li className="nav-item">Lorem item</li>
