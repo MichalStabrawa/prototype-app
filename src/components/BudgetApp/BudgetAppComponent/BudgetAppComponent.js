1740b4f2795fab692768fac4e1c663bfdcb748b8
MichalStabrawa <mistaaqua@gmail.com>
2023-01-27 00:41:17 +0100

    position last value

--- src/components/BudgetApp/BudgetAppComponent/BudgetAppComponent.js
+++ src/components/BudgetApp/BudgetAppComponent/BudgetAppComponent.js
@@ -228,8 +228,8 @@ const BudgetAppComponent = (props) => {
                         </div>
                         <div className={classes.exchange_item}>
                             <p className={classes.exchange_item__paragraph}>
-                                <span className={classes.text_bold}>{exchange[1]?.code}</span>
-                                {exchange[1]?.value}
+                                <span className={classes.last_wrapper}> <span className={classes.text_bold}>{exchange[1]?.code}</span>
+                                    {exchange[1]?.value}</span>
                                 <span
                                     className={`${classes.last_value} ${classes[getCurrentPrevDifferences(exchange[1]?.value,
                                         exchangeLast[1]?.value)]}`}>
@@ -240,8 +240,8 @@ const BudgetAppComponent = (props) => {
                                 />
                             </p>
                             <p className={classes.exchange_item__paragraph}>
-                                <span className={classes.text_bold}>{exchange[7]?.code}</span>
-                                {exchange[7]?.value}
+                                <span className={classes.last_wrapper}> <span className={classes.text_bold}>{exchange[7]?.code}</span>
+                                    {exchange[7]?.value}</span>
                                 <span
                                     className={`${classes.last_value} ${classes[getCurrentPrevDifferences(exchange[7]?.value,
                                         exchangeLast[7]?.value)]}`}>
@@ -252,10 +252,10 @@ const BudgetAppComponent = (props) => {
                                 />
                             </p>
                             <p className={classes.exchange_item__paragraph}>
-                                <span
+                                <span className={classes.last_wrapper}><span
                                     className={classes.text_bold}>
                                     {exchange[9]?.code}
-                                </span> {exchange[9]?.value}
+                                </span> {exchange[9]?.value}</span>
                                 <span
                                     className={`${classes.last_value} ${classes[getCurrentPrevDifferences(exchange[9]?.value, exchangeLast[9]?.value)]}`}>
                                     {(exchange[9]?.value - exchangeLast[9]?.value).toFixed(3)}
@@ -265,8 +265,8 @@ const BudgetAppComponent = (props) => {
                                 />
                             </p>
                             <p className={classes.exchange_item__paragraph}>
-                                <span
-                                    className={classes.text_bold}>{exchange[10]?.code}</span> {exchange[10]?.value} <span
+                                <span className={classes.last_wrapper}>  <span
+                                    className={classes.text_bold}>{exchange[10]?.code}</span> {exchange[10]?.value}</span>   <span
                                         className={`${classes.last_value} ${classes[getCurrentPrevDifferences(exchange[10]?.value,
                                             exchangeLast[10]?.value)]}`}>{(exchange[10]?.value -
                                                 exchangeLast[10]?.value).toFixed(3)}</span>
