b40a3e262cfa887ad6fdf8715e77a8da42ce6de1
MichalStabrawa <mistaaqua@gmail.com>
2023-02-09 23:12:07 +0100

    add budgetAppFilters

---
+++ src/components/BudgetApp/BudgetAppFiltersComponent/BudgetAppFIlters.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import Select from "../../UI/Select/Select";
+
+const BudgetAppFilters = (props) => {
+    return (
+        <div>
+            <Select>
+                <option>Date</option>
+            </Select>
+        </div>
+
+    )
+}
+
+export default BudgetAppFilters
