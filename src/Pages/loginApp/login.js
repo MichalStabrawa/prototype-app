d7fc0eb0a5cce7eae2d30efd01f13a769beec82b
MichalStabrawa <mistaaqua@gmail.com>
2023-05-03 22:24:00 +0200

    login register directory

---
+++ src/Pages/loginApp/login.js
@@ -0,0 +1,13 @@
+import react from 'react'
+import classes from './login.module.scss'
+
+const LoginApp = () => {
+    return (
+        <div className="login">
+            Login
+        </div>
+    )
+}
+
+export default LoginApp
+
