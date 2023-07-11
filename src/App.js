2ce420b0e1248efd7454165c2cee4b2d382d60a6
MichalStabrawa <mistaaqua@gmail.com>
2023-07-11 23:28:05 +0200

    add exchange rates page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Register from './Pages/registerApp/register';
 import FooterAppSection from './components/FooterAppSection/FooterAppSection';
 import RemindLogin from './Pages/remindLogin/remindLogin';
 import AboutUs from './Pages/about/aboutUs';
+import ExchangeRates from './Pages/exchange/exchange-rates';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/login/remind-login' element={<RemindLogin />} />
+        <Route path='/exchange' element={<ExchangeRates />} />
       </Routes>
       <FooterAppSection></FooterAppSection>
 
