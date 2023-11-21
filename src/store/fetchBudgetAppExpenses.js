ccdf31eab666363d25b7721631f781c6761a78f9
Michal Stabrawa <mistaaqua@gmail.com>
2023-11-22 00:11:33 +0100

    format code

--- src/store/fetchBudgetAppExpenses.js
+++ src/store/fetchBudgetAppExpenses.js
@@ -1,25 +1,24 @@
-const urlExpenses = 'https://budget-app-1a588-default-rtdb.firebaseio.com/expenses.json';
+const urlExpenses =
+  "https://budget-app-1a588-default-rtdb.firebaseio.com/expenses.json";
 const header = new Headers({ "Access-Control-Allow-Origin": "*" });
 
 const fetchBudgetAppExpenses = async function (expenses) {
-    try {
-        const response = await fetch(urlExpenses, {
-            method: 'POST',
-            body: JSON.stringify(expenses),
-            header: header,
-            headers: {
-                'Content-Type': 'aplication/json'
-            }
-
-        })
-        if (!response.ok) {
-            throw new Error('Somthing went wrong')
-        }
-        const data = await response.json();
-
-    } catch (error) {
-        console.log(error.message)
+  try {
+    const response = await fetch(urlExpenses, {
+      method: "POST",
+      body: JSON.stringify(expenses),
+      header: header,
+      headers: {
+        "Content-Type": "aplication/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Somthing went wrong");
     }
-}
+    const data = await response.json();
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-export default fetchBudgetAppExpenses
+export default fetchBudgetAppExpenses;
--- src/store/fetchNbpCurrentApi.js
+++ src/store/fetchNbpCurrentApi.js
@@ -1,38 +1,42 @@
-const url = 'http://api.nbp.pl/api/exchangerates/tables/A';
+const url = "http://api.nbp.pl/api/exchangerates/tables/A";
 const header = new Headers({ "Access-Control-Allow-Origin": "*" });
 
-const fethNbpCurrent = async function (setIsLoading, setError, setExchange, dispatchDate) {
-    setIsLoading(true)
-    setError(null)
+const fethNbpCurrent = async function (
+  setIsLoading,
+  setError,
+  setExchange,
+  dispatchDate
+) {
+  setIsLoading(true);
+  setError(null);
 
-    try {
-        const response = await fetch(url, {
-            header: header
-        })
-        if (!response.ok) {
-            throw new Error('Somthing went wrong')
-        }
-        const data = await response.json();
-        const currentDataNBP = data[0].effectiveDate;
-        const transformesExchange = data[0].rates.map(el => {
-
-            return {
-                name: el.currency,
-                code: el.code,
-                value: el.mid
-            }
-        })
-
-        setExchange(transformesExchange);
-        dispatchDate({
-            type: 'addCurentDate',
-            currentDate: currentDataNBP
-        })
-    } catch (error) {
-        setError(error.message)
-        console.log(error.message)
+  try {
+    const response = await fetch(url, {
+      header: header,
+    });
+    if (!response.ok) {
+      throw new Error("Somthing went wrong");
     }
-    setIsLoading(false)
-}
+    const data = await response.json();
+    const currentDataNBP = data[0].effectiveDate;
+    const transformesExchange = data[0].rates.map((el) => {
+      return {
+        name: el.currency,
+        code: el.code,
+        value: el.mid,
+      };
+    });
+
+    setExchange(transformesExchange);
+    dispatchDate({
+      type: "addCurentDate",
+      currentDate: currentDataNBP,
+    });
+  } catch (error) {
+    setError(error.message);
+    console.log(error.message);
+  }
+  setIsLoading(false);
+};
 
 export default fethNbpCurrent;
