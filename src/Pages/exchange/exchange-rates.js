b3d140739291a032d595be6fabbf0c7a44118dfb
Michal Stabrawa <mistaaqua@gmail.com>
2023-12-05 15:08:03 +0100

    add some changes format code etc

--- src/Pages/exchange/exchange-rates.js
+++ src/Pages/exchange/exchange-rates.js
@@ -3,31 +3,39 @@ import classes from "./exchange-rates.module.scss";
 import { useSelector } from "react-redux";
 import BudgetAppSection from "../../components/BudgetApp/BudgetAppSection/BudgetAppSection";
 const ExchangeRates = (props) => {
-  const currency = useSelector((state) => state.currency.data); 
+  const currency = useSelector((state) => state.currency.data);
   console.log("Currency Page");
-  console.log(Array.isArray(currency.rates) );
+  console.log(Array.isArray(currency.rates));
   return (
     <Wrapper>
-      <h1>Exchange Rates</h1>
+      <header>
+        <h1>Exchange Rates</h1>
+      </header>
       <div className={classes.exchange_wrapper}>
         <BudgetAppSection>
-          {" "}
-          <p>Table {currency.table}</p>
-          <p>DATA {currency.effectiveDate}</p>
           <div className={classes.table}>
+            <p>
+              Table: {currency.table}, date: {currency.effectiveDate}
+            </p>
+            <p></p>
             <table>
               <tr>
                 <th>Code</th>
                 <th>Currency</th>
                 <th>Value</th>
+                <th>Date</th>
               </tr>
-              {currency.rates &&currency.rates.map((el,index) =>{
-                return   <tr key={el.index}>
-                <td>{el.code}</td>
-                <td>{el.currency}</td>
-                <td>{el.mid}</td>
-              </tr>
-              } )}
+              {currency.rates &&
+                currency.rates.map((el, index) => {
+                  return (
+                    <tr key={el.index}>
+                      <td>{el.code}</td>
+                      <td>{el.currency}</td>
+                      <td>{el.mid}</td>
+                      <td>{currency.effectiveDate}</td>
+                    </tr>
+                  );
+                })}
             </table>
           </div>
         </BudgetAppSection>
