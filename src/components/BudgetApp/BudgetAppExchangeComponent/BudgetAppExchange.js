133b77f9da2b47d6dae1a9c8e10af7d90ff40662
Michal Stabrawa <mistaaqua@gmail.com>
2023-12-12 20:49:48 +0100

    add new table to main page

--- src/components/BudgetApp/BudgetAppExchangeComponent/BudgetAppExchange.js
+++ src/components/BudgetApp/BudgetAppExchangeComponent/BudgetAppExchange.js
@@ -28,6 +28,7 @@ import getCompareLastActualValue from "../../../utils/getCurrentLastValue";
 import Button from "../../UI/Button/Button";
 
 import ExchangeMainTable from "../../ExchangeComponents/ExchangeMainTable/ExchangeMainTable";
+import ExchangeTableMidValue from '../../ExchangeComponents/ExchangeTableMidValue/ExchangeTableMidValue';
 
 const { reducerDate, initialDate, fetchNbpTopCountReducer } = Reducer;
 
@@ -92,7 +93,7 @@ const BudgetAppExchange = (props) => {
       {isLoading && <p>Is Loading</p>}
       <div className={classes.exchange_item}>
         <div className={classes.exchange_item_current}>
-        <h3>Table {dataCurrencySelector[0].table}</h3>
+        <h3>Table</h3>
           <span>
             Current NBP {stateDate.currentDate} {getCurrentDate()}
           </span>
@@ -124,117 +125,9 @@ const BudgetAppExchange = (props) => {
         </div>
       </div>
       <div className={classes.exchange_item}>
-        <p className={classes.exchange_item__paragraph}>
-          <span className={classes.last_wrapper}>
-            {" "}
-            <span className={classes.text_bold}>{exchange[1]?.code}</span>
-            {exchange[1]?.value}
-          </span>
-          <span className={classes.last_date}>{stateDate.currentDate}</span>
-          <span
-            className={`${classes.last_value} ${
-              classes[
-                getCurrentPrevDifferences(
-                  exchange[1]?.value,
-                  exchangeLast[1]?.value
-                )
-              ]
-            }`}
-          >
-            {(exchange[1]?.value - exchangeLast[1]?.value).toFixed(3)}
-          </span>
-          <IconArrow
-            arrow={getCurrentPrevDifferences(
-              exchange[1]?.value,
-              exchangeLast[1]?.value
-            )}
-          />
-          <span>{exchangeLast[1]?.value}</span>
-          <span className={classes.last_date}>{stateDate.lastDate}</span>
-        </p>
-        <p className={classes.exchange_item__paragraph}>
-          <span className={classes.last_wrapper}>
-            {" "}
-            <span className={classes.text_bold}>{exchange[7]?.code}</span>
-            {exchange[7]?.value}
-          </span>
-          <span className={classes.last_date}>{stateDate.currentDate}</span>
-          <span
-            className={`${classes.last_value} ${
-              classes[
-                getCurrentPrevDifferences(
-                  exchange[7]?.value,
-                  exchangeLast[7]?.value
-                )
-              ]
-            }`}
-          >
-            {(exchange[7]?.value - exchangeLast[7]?.value).toFixed(3)}
-          </span>
-          <IconArrow
-            arrow={getCurrentPrevDifferences(
-              exchange[7]?.value,
-              exchangeLast[7]?.value
-            )}
-          />
-          <span>{exchangeLast[7]?.value}</span>
-          <span className={classes.last_date}>{stateDate.lastDate}</span>
-        </p>
-        <p className={classes.exchange_item__paragraph}>
-          <span className={classes.last_wrapper}>
-            <span className={classes.text_bold}>{exchange[9]?.code}</span>{" "}
-            {exchange[9]?.value}
-          </span>
-          <span className={classes.last_date}>{stateDate.currentDate}</span>
-          <span
-            className={`${classes.last_value} ${
-              classes[
-                getCurrentPrevDifferences(
-                  exchange[9]?.value,
-                  exchangeLast[9]?.value
-                )
-              ]
-            }`}
-          >
-            {(exchange[9]?.value - exchangeLast[9]?.value).toFixed(3)}
-          </span>
-          <IconArrow
-            arrow={getCurrentPrevDifferences(
-              exchange[9]?.value,
-              exchangeLast[9]?.value
-            )}
-          />
-          <span>{exchangeLast[9]?.value}</span>
-          <span className={classes.last_date}>{stateDate.lastDate}</span>
-        </p>
-        <p className={classes.exchange_item__paragraph}>
-          <span className={classes.last_wrapper}>
-            <span className={classes.text_bold}>{exchange[10]?.code} </span>
-            {exchange[10]?.value}
-          </span>
-          <span className={classes.last_date}>{stateDate.currentDate}</span>
-          <span
-            className={`${classes.last_value} ${
-              classes[
-                getCurrentPrevDifferences(
-                  exchange[10]?.value,
-                  exchangeLast[10]?.value
-                )
-              ]
-            }`}
-          >
-            {(exchange[10]?.value - exchangeLast[10]?.value).toFixed(3)}
-          </span>
-          <IconArrow
-            arrow={getCurrentPrevDifferences(
-              exchange[10]?.value,
-              exchangeLast[10]?.value
-            )}
-          />
-          <span>{exchangeLast[10]?.value}</span>
-          <span className={classes.last_date}>{stateDate.lastDate}</span>
-        </p>
-        <ExchangeMainTable />
+    {false &&  <ExchangeMainTable />}
+       
+        <ExchangeTableMidValue dataMid={data}/>
       </div>
       <div className={classes.chart}>
         <ResponsiveContainer width="100%" height="100%">
---
+++ src/components/ExchangeComponents/ExchangeTableMidValue/ExchangeTableMidValue.js
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import classes from "./ExchangeTableMidValue.module.scss";
+import getCurrentPrevDifferences from "../../../utils/getCurrentPrevDifferences";
+import IconArrow from "../../UI/iconArrow/iconArrow";
+
+const ExchangeTableMidValue = (props) => {
+  const data = useSelector((state) => state.multiple.data);
+  const status = useSelector((state) => state.multiple.status);
+  const [newData, setNewData] = useState([
+    { code: "", currency: "", rates: [{ bid: "", ask: "" }] },
+  ]);
+
+  const{dataMid}=props
+
+  useEffect(() => {
+    console.log("Status Table");
+    console.log("Data tMid");
+    console.log(data);
+    setNewData(data);
+  }, [data]);
+  return (
+    <div>
+      <table className={classes.table_rates}>
+        <thead>
+          <tr>
+            <th>code</th>
+            <th>currency</th>
+            <th>mid value</th>
+            <th>rate</th>
+            <th>rate arrow</th>
+            <th>date</th>
+            <th>lastValue</th>
+            <th>last date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {status === "success" &&
+            dataMid &&
+            dataMid.map((el, index) => {
+
+                if(el.code ==='USD' || el.code==='EUR' || el.code ==='CHF' || el.code ==='GBP') {
+                    return (
+                        <tr key={index}>
+                          <td className={classes.code}>{el.code}</td>
+                          <td>{el.currency}</td>
+                          <td className={classes.bold}>{el.mid}</td>
+                          <td           className={`${classes.rate} ${
+                        classes[getCurrentPrevDifferences(el.mid, el.lastValue)]
+                      }`}>{(el.mid - el.lastValue).toFixed(4)}</td>
+                          <td>
+                      <IconArrow
+                        arrow={getCurrentPrevDifferences(el.mid, el.lastValue)}
+                      />
+                    </td>
+                          <td className={classes.date}>{data[0].rates[9].effectiveDate}</td>
+                          <td>{el.lastValue}</td>
+                          <td className={classes.date}>{data[0].rates[8].effectiveDate}</td>
+                        </tr>
+                      );
+                }else {
+                    return null
+                }
+            
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ExchangeTableMidValue;
