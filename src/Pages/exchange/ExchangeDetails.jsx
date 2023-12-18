c2f7bab2ff68d422ce3da8979c7c60e6f1e4bdf9
Michal Stabrawa <mistaaqua@gmail.com>
2023-12-18 22:48:45 +0100

    add changes in ExchangeDetail component

--- src/Pages/exchange/ExchangeDetails.jsx
+++ src/Pages/exchange/ExchangeDetails.jsx
@@ -1,12 +1,46 @@
-import {useParams} from 'react-router-dom'
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 
+import Wrapper from "../../components/UI/Wrapper/Wrapper";
 
 function ExchangeDetails() {
-    const params = useParams()
-    console.log(params.id)
-    return(<><h1>Exchange Details</h1>{params.id}</>
-        
-    )
+  const params = useParams();
+  const currency = useSelector((state) => state.currency.data);
+  console.log(params.id);
+  console.log("Currency isnide EUR");
+  console.log(currency);
+
+  const filterCurrency = currency[1].rates.filter(
+    (el) => el.code === params.id
+  );
+  console.log(filterCurrency);
+
+  return (
+    <>
+      <Wrapper>
+        <header>
+          {" "}
+          <h1>Exchange Details</h1>
+        </header>
+      </Wrapper>
+      <Wrapper css="grid">
+        <main className="exchange">
+          <div>
+            <h3>{filterCurrency[0].code}</h3>
+            <p>
+              {filterCurrency[0].currency} <span>{filterCurrency[0].mid}</span>
+            </p>
+          </div>
+          <div>
+            <div className="tabulation">
+              <button>5d</button><button>7d</button><button>14d</button>
+            </div>
+          </div>
+        </main>
+      </Wrapper>
+    </>
+  );
 }
 
-export default ExchangeDetails
+export default ExchangeDetails;
