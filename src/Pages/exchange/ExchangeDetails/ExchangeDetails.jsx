646a62fb5daf8de94ae8271e39dcec1b51a809a0
Michal Stabrawa <mistaaqua@gmail.com>
2023-12-21 18:36:02 +0100

    add chart for data single currnecy side

--- src/Pages/exchange/ExchangeDetails/ExchangeDetails.jsx
+++ src/Pages/exchange/ExchangeDetails/ExchangeDetails.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
@@ -11,26 +11,77 @@ import Col from "react-bootstrap/Col";
 import Spinner from "react-bootstrap/Spinner";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
+import Alert from 'react-bootstrap/Alert';
+import IconArrow from "../../../components/UI/iconArrow/iconArrow";
+import getCurrentPrevDifferences from '../../../utils/getCurrentPrevDifferences';
+
+import { singleCurrencyLastFewTimes } from "../../../store/currencyApiNbp/singleCurrencyLastFewTimes";
+import { TiArrowBackOutline } from "react-icons/ti";
+import {
+  LineChart,
+  BarChart,
+  Rectangle,
+  Bar,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 import classes from "./ExchangeDetails.module.scss";
 
 function ExchangeDetails() {
+  const dispatch = useDispatch();
   const params = useParams();
   const currency = useSelector((state) => state.currency.data);
   const status = useSelector((state) => state.currency.status);
   const isLoading = useSelector((state) => state.currency.isLoading);
   const [data, setData] = useState();
+  const [dataLast,setDataLast] = useState();
   const [key, setKey] = useState("3");
+
+  //ask bid data
+  const currencyLastTopCount = useSelector(
+    (state) => state.singleCurrencyLastFewTimes.data
+  );
+  const isLoadingLastTop = useSelector(
+    (state) => state.singleCurrencyLastFewTimes.isLoading
+  );
+
+  const statusLastTop = useSelector(
+    (state) => state.singleCurrencyLastFewTimes.status
+  );
+  const errorLast = useSelector((state)=>state.singleCurrencyLastFewTimes.error)
+
+  const [number, setNumber] = useState(3);
 
   const filterCurrency = (data) => {
     return data[1].rates.filter((el) => el.code === params.id);
+  };
+  const filterCurrencyLast = (data) => {
+    return data[0].rates.filter((el) => el.code === params.id);
   };
 
   useEffect(() => {
     if (status === "success") {
       setData(filterCurrency(currency));
+      setDataLast(filterCurrencyLast(currency))
     }
   }, [currency]);
+
+  useEffect(() => {
+    if (params.id !== "") {
+      dispatch(
+        singleCurrencyLastFewTimes({
+          code: params.id,
+          number: +key,
+        })
+      );
+    }
+  }, [key, params.id]);
 
   if (isLoading) {
     return (
@@ -46,28 +97,34 @@ function ExchangeDetails() {
         <header>
           {" "}
           <h1>Exchange Details</h1>
+          {errorLast}
         </header>
       </Wrapper>
       <Wrapper css="grid">
-        <Container></Container>
-        <main className="exchange">
-          <Container>
+      
+        <main className={classes.exchange}>
+          <Container fluid>
             <Row>
-              <Col>
-                {" "}
-                <Link to="/exchange">
+              <Col >
+                <div className={classes.back}>
                   {" "}
-                  <Button variant="secondary">Back</Button>
-                </Link>
+                  <Link to="/exchange">
+                    {" "}
+                    <Button variant="secondary"><TiArrowBackOutline /> Back</Button>
+                  </Link>
+                </div>
               </Col>
             </Row>
             <Row>
-              <Col>
+              <Col xs={12} md={3}>
                 {currency && data && (
                   <div>
-                    <h3>{data[0].code}</h3>
-                    <p>
-                      {data[0].currency} <span>{data[0].mid}</span>
+                    <h3 className={classes.code}>{data[0].code}</h3>
+                    <p className={classes.value}>
+                      <span>{data[0].mid}</span> <IconArrow  arrow={getCurrentPrevDifferences(data[0].mid, dataLast[0].mid)}/>
+                    </p>
+                    <p> {data[0].currency} </p>
+                    <p className={classes.date}>
                       <span>date: {currency[1].effectiveDate}</span>
                     </p>
                   </div>
@@ -80,16 +137,48 @@ function ExchangeDetails() {
                   onSelect={(k) => setKey(k)}
                   className="mb-3"
                 >
-                  <Tab eventKey="3" title="3D">
-                    Tab content for 3
-                  </Tab>
-                  <Tab eventKey="7" title="7D">
-                    Tab content for 7
-                  </Tab>
-                  <Tab eventKey="14" title="14D">
-                    Tab content for 14
-                  </Tab>
+                  <Tab eventKey="3" title="3D"></Tab>
+                  <Tab></Tab>
+                  <Tab eventKey="14" title="14D"></Tab>
+                  <Tab eventKey="21" title="21D"></Tab>
+                  <Tab eventKey="30" title="1m"></Tab>
+                  <Tab eventKey="60" title="2m"></Tab>
                 </Tabs>
+                <Row>
+                  <Col>
+                  {errorLast&& <Alert variant="warning">Error fetch data</Alert>}
+                    {statusLastTop === "success" && (
+                      <div className={classes.tab_content}>
+                        <ResponsiveContainer width="100%" height="100%">
+                          <LineChart
+                            width={500}
+                            height={300}
+                            data={currencyLastTopCount.rates}
+                            margin={{
+                              top: 5,
+                              right: 30,
+                              left: 20,
+                              bottom: 5,
+                            }}
+                          >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="effectiveDate" />
+                            <YAxis domain={["dataMin"]} />
+                            <Tooltip />
+                            <Legend />
+                            <Line
+                              type="linear"
+                              dataKey="bid"
+                              stroke="blue"
+                              activeDot={{ r: 8 }}
+                            />
+                            <Line type="linear" dataKey="ask" stroke="violet" />
+                          </LineChart>
+                        </ResponsiveContainer>
+                      </div>
+                    )}
+                  </Col>
+                </Row>
               </Col>
             </Row>
           </Container>
--- src/components/UI/iconArrow/iconArrow.js
+++ src/components/UI/iconArrow/iconArrow.js
@@ -8,6 +8,8 @@ import ArrowRight from '../../../assets/icons8-right-48.png';
 const IconArrow = (props) => {
     const { arrow } = props
     const [src, setSrc] = useState('');
+    console.log('ARROW');
+    console.log(arrow)
     const getSrc = () => {
         if (arrow === null) {
             setSrc(ArrowRight)
