6d2b00ef005a4f29810352a75d1c2fb4d6b36e29
Michal Stabrawa <mistaaqua@gmail.com>
2023-12-26 22:23:23 +0100

    add col

--- src/Pages/exchange/ExchangeDetails/ExchangeDetails.jsx
+++ src/Pages/exchange/ExchangeDetails/ExchangeDetails.jsx
@@ -184,7 +184,7 @@ function ExchangeDetails() {
                   <Tab eventKey="180" title="6m"></Tab>
                 </Tabs>
                 <Row>
-                  <Col>
+                  <Col xs={12}>
                     {errorLast && (
                       <Alert variant="warning">Error fetch data</Alert>
                     )}
@@ -216,36 +216,34 @@ function ExchangeDetails() {
                             <Line type="linear" dataKey="ask" stroke="violet" />
                           </LineChart>
                         </ResponsiveContainer>
-                        {minBidAsk &&
-                          maxBidAsk &&
-                          statusLastTop === "success" && (
-                            <div className={classes.min_max}>
-                              <p className={classes.content}>
-                                {" "}
-                                <span className={classes.min}>
-                                  {" "}
-                                  min bid:{" "}
-                                </span>{" "}
-                                {minBidAsk.bid}{" "}
-                                <span className={classes.min}>min ask:</span>{" "}
-                                {minBidAsk.ask}{" "}
-                                <span className={classes.date_min_max}>
-                                  {" "}
-                                  date: {minBidAsk.effectiveDate}
-                                </span>
-                              </p>
-                              <p>
-                                <span className={classes.max}>max bid: </span>{" "}
-                                {maxBidAsk.bid}{" "}
-                                <span className={classes.max}> max ask :</span>
-                                {maxBidAsk.ask}{" "}
-                                <span className={classes.date_min_max}>
-                                  {" "}
-                                  date: {maxBidAsk.effectiveDate}
-                                </span>
-                              </p>
-                            </div>
-                          )}
+                      </div>
+                    )}
+                  </Col>
+                  <Col xs={12}>
+                    {" "}
+                    {minBidAsk && maxBidAsk && statusLastTop === "success" && (
+                      <div className={classes.min_max}>
+                        <p className={classes.content}>
+                          {" "}
+                          <span className={classes.min}> min bid: </span>{" "}
+                          {minBidAsk.bid}{" "}
+                          <span className={classes.min}>min ask:</span>{" "}
+                          {minBidAsk.ask}{" "}
+                          <span className={classes.date_min_max}>
+                            {" "}
+                            date: {minBidAsk.effectiveDate}
+                          </span>
+                        </p>
+                        <p>
+                          <span className={classes.max}>max bid: </span>{" "}
+                          {maxBidAsk.bid}{" "}
+                          <span className={classes.max}> max ask :</span>
+                          {maxBidAsk.ask}{" "}
+                          <span className={classes.date_min_max}>
+                            {" "}
+                            date: {maxBidAsk.effectiveDate}
+                          </span>
+                        </p>
                       </div>
                     )}
                   </Col>
