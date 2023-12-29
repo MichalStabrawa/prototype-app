710cd7fabd4f4a641bb583883e07c8857fad2e3b
Michal Stabrawa <mistaaqua@gmail.com>
2023-12-29 21:57:49 +0100

    bidask some style

--- src/Pages/bidask/bidAsk.js
+++ src/Pages/bidask/bidAsk.js
@@ -1,19 +1,21 @@
 import BudgetAppSection from "../../components/BudgetApp/BudgetAppSection/BudgetAppSection";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Wrapper from "../../components/UI/Wrapper/Wrapper";
+import classes from "./bidask.module.scss";
 
 function BidAsk() {
-  return(<BudgetAppSection>
-    <header>
-        <Container fluid>
-            <Row>
-                <Col>
-                <h1>Bid & Ask currency</h1></Col>
-            </Row>
-        </Container>
-    </header>
-  </BudgetAppSection>) 
+  return (
+    <Wrapper>
+      <header className={classes.header}>
+        <h1>Bid & Ask currency</h1>
+      </header>
+      <main className={classes}>
+        <BudgetAppSection title="Sell ​​or buy currencies"></BudgetAppSection>
+      </main>
+    </Wrapper>
+  );
 }
 
 export default BidAsk;
