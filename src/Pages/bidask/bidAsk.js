d579231ce029e91ac449a7123c1651179532bf74
Michal Stabrawa <mistaaqua@gmail.com>
2023-12-30 12:46:57 +0100

    add select and input bootstrap

--- src/Pages/bidask/bidAsk.js
+++ src/Pages/bidask/bidAsk.js
@@ -7,19 +7,20 @@ import Col from "react-bootstrap/Col";
 import Wrapper from "../../components/UI/Wrapper/Wrapper";
 import classes from "./bidask.module.scss";
 import InputComponent from "../../components/UI/Input/InputComponent";
-import Select from "../../components/UI/Select/Select";
+import Form from "react-bootstrap/Form";
+import Button from 'react-bootstrap/Button'
 import { fetchNbpTableC } from "../../store/currencyApiNbp/currencyFetchTableC";
 
 function BidAsk() {
   const dispatch = useDispatch();
   const data = useSelector((state) => state.tableC.data);
-  const status = useSelector(state=>state.tableC.status)
-  const isLoading = useSelector(state=>state.tableC.isLoading)
+  const status = useSelector((state) => state.tableC.status);
+  const isLoading = useSelector((state) => state.tableC.isLoading);
 
-  console.log(data)
-  useEffect(()=> {
-    dispatch(fetchNbpTableC())
-  },[dispatch])
+  console.log(data);
+  useEffect(() => {
+    dispatch(fetchNbpTableC());
+  }, [dispatch]);
 
   if (isLoading) {
     return "LOADING......";
@@ -33,14 +34,31 @@ function BidAsk() {
         </header>
       </Wrapper>
       <Wrapper>
-        <main className={classes}>
+        <main className={classes.main}>
           <Container fluid>
             <Row>
               <Col>
                 <h3>Sell or buy currency</h3>
+                <Col md={6}>
+                  <Form.Control type="number" placeholder="count" />
+                  <Form.Select aria-label="Default select example">
+                    <option>Open this select menu</option>
+                    {data[0].rates.map((el, index) => (
+                      <option
+                        value={el.bid}
+                        key={index}
+                        data-names={el.currency}
+                        data-code={el.code}
+                      >
+                        {el.code} /{el.currency}
+                      </option>
+                    ))}
+                  </Form.Select>
+                  <Button variant="secondary">ask >></Button>
+                </Col>
               </Col>
               <Col>
-                <h3>Table {status==='success' && data[0].table}</h3>
+                <h3>Table {status === "success" && data[0].table}</h3>
               </Col>
             </Row>
           </Container>
