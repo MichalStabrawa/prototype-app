c6643fa9b03a03397500ae2d2672b3bab690f7c5
Michal Stabrawa <mistaaqua@gmail.com>
2024-01-04 17:08:10 +0100

    add style to table

--- src/components/CalculateGoldPriceByDateComponent/CompareGoldPricesByDate.jsx
+++ src/components/CalculateGoldPriceByDateComponent/CompareGoldPricesByDate.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import classes from "./CompareGoldPricesByDate.module.scss";
 import { goldFetchDate } from "../../store/goldApiNbp/goldFetchDateSlice";
-import getCurrentPrevDifferences from '../../utils/getCurrentPrevDifferences';
+import getCurrentPrevDifferences from "../../utils/getCurrentPrevDifferences";
 
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
@@ -53,36 +53,52 @@ const CompareGoldPricesByDate = () => {
       {statusGoldDate === "error" && (
         <Alert variant="warning">Error fetch data</Alert>
       )}
-   
-        {statusGoldDate === "success" && status ==="success"&&(
-          <div className={classes.table_min_max}>
-            {" "}
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>current value</th>
-                  <th>date</th>
-                  <th>selected value</th>
-                  <th>selected date</th>
-                  <th>amount</th>
-                  <th>%</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td className={classes.min}> {gold[1].cena}</td>
-                  <td className={classes.date_min_max}>{gold[1].data}</td>
-                  <td className={classes.max}>{goldDate[0].cena}</td>
-                  <td className={classes.date_min_max}>{goldDate[0].data}</td>
-                  <td className={`${classes[getCurrentPrevDifferences(gold[1].cena,goldDate[0].cena)]}`}>{(gold[1].cena-goldDate[0].cena).toFixed(4)}</td>
-                  <td className={`${classes[getCurrentPrevDifferences(gold[1].cena,goldDate[0].cena)]}`}>{((((100*gold[1].cena)/goldDate[0].cena))-100).toFixed(4)+"%"}</td>
-                </tr>
-              </tbody>
-            </Table>
-            <label>Gold price = PLN/1g</label>
-          </div>
-        )}
-    
+
+      {statusGoldDate === "success" && status === "success" && (
+        <div className={classes.table}>
+          {" "}
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>current value</th>
+                <th>date</th>
+                <th>selected value</th>
+                <th>selected date</th>
+                <th>amount</th>
+                <th>%</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className={classes.value}> {gold[1].cena}</td>
+                <td className={classes.date}>{gold[1].data}</td>
+                <td className={classes.value}>{goldDate[0].cena}</td>
+                <td className={classes.date}>{goldDate[0].data}</td>
+                <td
+                  className={`${
+                    classes[
+                      getCurrentPrevDifferences(gold[1].cena, goldDate[0].cena)
+                    ]
+                  }`}
+                >
+                  {(gold[1].cena - goldDate[0].cena).toFixed(4)}
+                </td>
+                <td
+                  className={`${
+                    classes[
+                      getCurrentPrevDifferences(gold[1].cena, goldDate[0].cena)
+                    ]
+                  }`}
+                >
+                  {((100 * gold[1].cena) / goldDate[0].cena - 100).toFixed(4) +
+                    "%"}
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+          <label>Gold price = PLN/1g</label>
+        </div>
+      )}
     </div>
   );
 };
