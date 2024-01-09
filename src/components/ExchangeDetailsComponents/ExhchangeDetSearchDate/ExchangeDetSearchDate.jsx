b535f91c0fadb01299e14cd07623187c75993db1
Michal Stabrawa <mistaaqua@gmail.com>
2024-01-09 12:27:48 +0100

    add table

--- src/components/ExchangeDetailsComponents/ExhchangeDetSearchDate/ExchangeDetSearchDate.jsx
+++ src/components/ExchangeDetailsComponents/ExhchangeDetSearchDate/ExchangeDetSearchDate.jsx
@@ -1,79 +1,125 @@
-import {useState} from 'react'
+import { useSelector, useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
 import classes from "./ExchangeDetSearchDate.module.scss";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Form from 'react-bootstrap/Form';
-import Table from 'react-bootstrap/Table'
+import Form from "react-bootstrap/Form";
+import Table from "react-bootstrap/Table";
 
-function ExchangeDetSearchDate({data,currency}) {
-  const [lastDate,setLastDate] = useState();
+import { singleCurrencyDateFetch } from "../../../store/currencyApiNbp/singleCurrencyFetchDateSlice";
+import getCurrentPrevDifferences from "../../../utils/getCurrentPrevDifferences";
 
-  const handleInputDate = (e)=> {
-    setLastDate(e.target.value)
-  }
+function ExchangeDetSearchDate({ data, currency }) {
+  const dispatch = useDispatch();
+  const fetchData = useSelector((state) => state.singleCurrency.data);
+  const isLoading = useSelector((state) => state.singleCurrency.isLoading);
+  const status = useSelector((state) => state.singleCurrency.status);
+  const [lastDate, setLastDate] = useState();
+
+  const handleInputDate = (e) => {
+    setLastDate(e.target.value);
+  };
+
+  console.log("DATA TABLE");
+  console.log(data);
+  console.log(currency);
+
+  useEffect(() => {
+    if (data && currency)
+      dispatch(
+        singleCurrencyDateFetch({
+          table: currency[1].table,
+          code: data[0].code,
+          date: lastDate,
+        })
+      );
+  }, [lastDate, dispatch, currency, data]);
   return (
     <div className={classes.details_wrapper}>
       <Row>
         <Col>
-          <h2 className={classes.title}>Search currency {} to date</h2>
+          <h2 className={classes.title}>
+            Search currency to date for {data[0].code} {data[0].currency}
+          </h2>
         </Col>
       </Row>
       <Row>
         <Col xs={12} md={3}>
-        <Form.Control
-        style={{ width: "100%" }}
-        type="date"
-        onChange={handleInputDate}
-      ></Form.Control>
-      <Form.Label>
-        <span className={classes.label}>
-          Select a date and check the value.
-        </span>
-      </Form.Label>
+          <Form.Control
+            style={{ width: "100%" }}
+            type="date"
+            onChange={handleInputDate}
+          ></Form.Control>
+          <Form.Label>
+            <span className={classes.label}>
+              Select a date and check the value.
+            </span>
+          </Form.Label>
         </Col>
         <Col>
-        <div className={classes.table}>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>current value</th>
-                <th>date</th>
-                <th>selected value</th>
-                <th>selected date</th>
-                <th>amount</th>
-                <th>%</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className={classes.value}>{ data[0].mid} </td>
-                {/* <td className={classes.date}>{gold[1].data}</td> */}
-                <td className={classes.value}>{currency[1].effectiveDate}</td>
-                <td className={classes.date}>{lastDate}</td>
-                {/* <td
-                  className={`${
-                    classes[
-                      getCurrentPrevDifferences(gold[1].cena, goldDate[0].cena)
-                    ]
-                  }`}
-                > */}
-                  {/* {(gold[1].cena - goldDate[0].cena).toFixed(4)} */}
-               {/* </tr> </td> */}
-                {/* <td
-                  className={`${
-                    classes[
-                      getCurrentPrevDifferences(gold[1].cena, goldDate[0].cena)
-                    ]
-                  }`}
-                >
-                  {((100 * gold[1].cena) / goldDate[0].cena - 100).toFixed(4) +
-                    "%"}
-                </td> */}
-              </tr>
-            </tbody>
-          </Table>
-          <label>Last selected date {lastDate}</label>
-        </div></Col>
+          <div className={classes.table}>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>current value</th>
+                  <th>date</th>
+                  <th>selected date</th>
+                  <th>selected value</th>
+
+                  <th>amount</th>
+                  <th>%</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className={classes.value}>{data[0].mid} </td>
+                  {/* <td className={classes.date}>{gold[1].data}</td> */}
+                  <td className={classes.value}>{currency[1].effectiveDate}</td>
+                  <td className={classes.date}>{lastDate}</td>
+                  <td className={classes.date}>
+                    {status === "success" && fetchData.rates[0].mid}
+                  </td>
+                  <td
+                    className={`${
+                      classes[
+                        `${
+                          status === "succes" &&
+                          getCurrentPrevDifferences(
+                            data[0].mid,
+                            fetchData.rates[0].mid
+                          )
+                        }`
+                      ]
+                    }`}
+                  >
+                    {status === "success" &&
+                      (data[0].mid, fetchData.rates[0].mid).toFixed(4)}
+                  </td>
+                  <td
+                    className={`${
+                      classes[
+                        `${
+                          status === "succes" &&
+                          getCurrentPrevDifferences(
+                            data[0].mid,
+                            fetchData.rates[0].mid
+                          )
+                        }`
+                      ]
+                    }`}
+                  >
+                    {status === "success" &&
+                      (
+                        (100 * data[0].mid) / fetchData.rates[0].mid -
+                        100
+                      ).toFixed(4) + "%"}
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+            <label>Last selected date {lastDate}</label>
+          </div>
+        </Col>
       </Row>
     </div>
   );
