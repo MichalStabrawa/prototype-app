813a5ccc9dbb17fa41ee08805f05dce0dc7e5f0b
Michal Stabrawa <mistaaqua@gmail.com>
2024-01-10 19:53:44 +0100

    add useLocation to footer

--- src/components/FooterAppSection/FooterAppSection.js
+++ src/components/FooterAppSection/FooterAppSection.js
@@ -1,24 +1,34 @@
-import { Link } from 'react-router-dom'
-import classes from './FooterAppSection.module.scss';
-import buttonStyles from '../UI/Button/Button.module.scss';
-
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import { Link } from "react-router-dom";
+import classes from "./FooterAppSection.module.scss";
+import buttonStyles from "../UI/Button/Button.module.scss";
+import { useLocation } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const FooterAppSection = () => {
-    return (<footer className={classes.footer}>
-         <Card className="text-center"  bg="dark" text='white'>
-      <Card.Header>BApp</Card.Header>
-      <Card.Body>
-        <Card.Title>Special title treatment</Card.Title>
-        <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
-        </Card.Text>
-        <Button variant="primary">Register</Button>
-      </Card.Body>
-      <Card.Footer className="text-muted">2 days ago</Card.Footer>
-    </Card>
-    </footer>)
-}
+  const location = useLocation();
+  console.log(location);
+  return (
+    <footer className={classes.footer}>
+      <Card className="text-center" bg="dark" text="white">
+        <Card.Header>BApp</Card.Header>
+        <Card.Body>
+          <Card.Title>Special title treatment</Card.Title>
+          <Card.Text>
+            With supporting text below as a natural lead-in to additional
+            content.
+          </Card.Text>
+          <Link to="register">
+            {" "}
+            <Button variant="primary">
+              {location.pathname === "/register" ? "Login" : "Register"}
+            </Button>
+          </Link>
+        </Card.Body>
+        <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      </Card>
+    </footer>
+  );
+};
 
-export default FooterAppSection
+export default FooterAppSection;
