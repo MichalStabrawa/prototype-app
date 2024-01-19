d061f4ac8ad2e1a1d9f5f5d4dca85ab1e1594ee6
Michal Stabrawa <mistaaqua@gmail.com>
2024-01-19 18:01:32 +0100

    add new test

---
+++ src/components/HeaderComponent/HeaderComponent.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import "matchmedia-polyfill";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Provider } from "react-redux"; // Import the Provider
+import configureStore from "redux-mock-store"; // You may need to install this package
+
+import HeaderComponent from "./HeaderComponent";
+
+const mockStore = configureStore();
+const store = mockStore({ currency: { data: null, status: "idle" } });
+
+test("Header render", () => {
+  render(
+    <Provider store={store}>
+      <HeaderComponent />
+    </Provider>
+  );
+});
