41f7b64d47b5410150475815a7b76e3d69fe2621
Michal Stabrawa <mistaaqua@gmail.com>
2024-02-16 16:20:36 +0100

    add new test

--- src/components/HeaderComponent/HeaderComponent.test.js
+++ src/components/HeaderComponent/HeaderComponent.test.js
@@ -1,13 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import "matchmedia-polyfill";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { Provider } from "react-redux"; // Import the Provider
-import configureStore from "redux-mock-store"; // You may need to install this package
-
+import { Provider } from "react-redux";
 import HeaderComponent from "./HeaderComponent";
-import ResponsiveCarousel from "../Carousel/ResponsiveCarousel/ResponsiveCarousel";
+import configureStore from "redux-mock-store";
 
 const mockStore = configureStore();
 const store = mockStore({ currency: { data: null, status: "idle" } });
