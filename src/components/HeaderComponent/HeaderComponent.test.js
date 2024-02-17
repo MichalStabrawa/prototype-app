221603877de889bfed9dbd082a8881370501d2b4
Michal Stabrawa <mistaaqua@gmail.com>
2024-02-17 22:26:08 +0100

    headerComponent.test

--- src/components/HeaderComponent/HeaderComponent.test.js
+++ src/components/HeaderComponent/HeaderComponent.test.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
-import store from '../../store/index';
-import HeaderComponent from './HeaderComponent';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom"; // Import BrowserRouter
+import store from "../../store/index";
+import HeaderComponent from "./HeaderComponent";
 
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
   Link: jest.fn().mockImplementation(({ children }) => <div>{children}</div>), // Mock Link component
 }));
 
-describe('HeaderComponent', () => {
+describe("HeaderComponent", () => {
   test('renders h1 with text "Budget APP"', () => {
     render(
       <Provider store={store}>
-        <Router> {/* Wrap HeaderComponent with BrowserRouter */}
+        <Router>
+          {" "}
+          {/* Wrap HeaderComponent with BrowserRouter */}
           <HeaderComponent scrollToRef={jest.fn()} />
         </Router>
       </Provider>
     );
-
-    const headerElement = screen.getByText('Budget APP');
+    console.log(screen.debug());
+    const headerElement = screen.getByText("Budget APP");
     expect(headerElement).toBeInTheDocument();
+    const additionalTextElement = screen.getByText("Check currency and gold rates, compare currencies, buy and sell. Manage your household budget");
+    expect(additionalTextElement).toBeInTheDocument();
   });
 });
