bb95adcbcec0d04fad2fb0e5b95488258789a482
Michal Stabrawa <mistaaqua@gmail.com>
2024-02-23 14:49:42 +0100

    add test kindOfTable

--- src/store/currencyApiNbp/KindOfTableSlice.test.js
+++ src/store/currencyApiNbp/KindOfTableSlice.test.js
@@ -1,21 +1,28 @@
-import { kindOfTableActions, default as kindOfTableReducer } from './kindOfTableSlice';
+import {
+  kindOfTableActions,
+  default as kindOfTableReducer,
+} from "./kindOfTableSlice";
 
-describe('kindOfTableSlice reducer', () => {
-    it('should return the initial state', () => {
-      expect(kindOfTableReducer(undefined, {})).toEqual({ table: 'A' });
-    });
-  
-    it('should handle changeToTableA', () => {
-      const prevState = { table: 'B' };
-      const nextState = kindOfTableReducer(prevState, kindOfTableActions.changeToTableA());
-      expect(nextState).toEqual({ table: 'A' });
-    });
-  
-    it('should handle changeToTableB', () => {
-      const prevState = { table: 'A' };
-      const nextState = kindOfTableReducer(prevState, kindOfTableActions.changeToTableB());
-      expect(nextState).toEqual({ table: 'B' });
-    });
-  
-    // You can add more tests as needed to cover other scenarios and edge cases
+describe("kindOfTableSlice reducer", () => {
+  it("should return the initial state", () => {
+    expect(kindOfTableReducer(undefined, {})).toEqual({ table: "A" });
   });
+
+  it("should handle changeToTableA", () => {
+    const prevState = { table: "B" };
+    const nextState = kindOfTableReducer(
+      prevState,
+      kindOfTableActions.changeToTableA()
+    );
+    expect(nextState).toEqual({ table: "A" });
+  });
+
+  it("should handle changeToTableB", () => {
+    const prevState = { table: "A" };
+    const nextState = kindOfTableReducer(
+      prevState,
+      kindOfTableActions.changeToTableB()
+    );
+    expect(nextState).toEqual({ table: "B" });
+  });
+});
