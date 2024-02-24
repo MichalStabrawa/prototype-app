dab32c2563f3aca168ada18af5150b2ecc7e0838
Michal Stabrawa <mistaaqua@gmail.com>
2024-02-24 22:38:17 +0100

    add state remind password

--- src/Pages/remindLogin/remindLogin.jsx
+++ src/Pages/remindLogin/remindLogin.jsx
@@ -7,14 +7,15 @@ import Button from '../../components/UI/Button/Button';
 import {Link} from 'react-router-dom';
 
 const RemindLogin = (props) => {
+    const [email,setEmail] = useState('')
     return (
         <div className={loginStyles.login}>
             <Wrapper>
                 <div className={loginStyles.login__wrapper}>
                     <h1>Remind user
                         Your username will be sent to you on the setup email address upon registration</h1>
-                    <InputComponent name='Remind login' />
-                    <Button name='Submit' />
+                    <InputComponent name='Remind password' />
+                    <Button name='Submit' disabled={email===''}/>
                    <Link to=".." relative='path'>Back</Link>
                 </div>
 
